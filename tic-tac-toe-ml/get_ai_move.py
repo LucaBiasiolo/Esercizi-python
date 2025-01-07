a9c9439b891d061e36c7b706f2ad2d66612f0c0a
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-01-07 17:13:05 +0100

    Switching to scikit-learn because of difficult error in tensorflow

--- tic-tac-toe-ml/get_ai_move.py
+++ tic-tac-toe-ml/get_ai_move.py
@@ -1,24 +1,20 @@
+from sklearn.neural_network import MLPClassifier
+import joblib
 import random
-import numpy
-import tensorflow as tf
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, Input
 
 def get_ai_move(game_matrix, machine_symbol):
     empty_spots = [(i, j) for i in range(3) for j in range(3) if game_matrix[i,j] == 0]
     return random.choice(empty_spots)
 
 def get_model():
-    # Placeholder for the neural network call
-    # This function should return the coordinates (row, col) for the AI move
-    model = Sequential([Dense(128, activation='relu', input_shape=(3,3)),
-                        Dense(64, activation='relu'),
-                        Dense(9, activation='linear')])
-    
-    model.compile(optimizer='adam',loss='mse')
-
+    model = MLPClassifier(hidden_layer_sizes=(128, 64), activation='relu', solver='adam', max_iter=500)
     return model
+
+def train_model(model):
+    pass
+    # TODO: insert logic for training here
 
 if __name__ == '__main__':
     model = get_model()
-    print(model(numpy.zeros((3,3), dtype=int)).numpy())
+    joblib.dump(model, './Esercizi/tic-tac-toe-ml/tic_tac_toe_mlp.pkl')
+    # print(model(np.ones((3, 3), dtype=int)).numpy())
--- tic-tac-toe-ml/tic_tac_toe_ml.py
+++ tic-tac-toe-ml/tic_tac_toe_ml.py
@@ -1,7 +1,10 @@
+import joblib
 import random
 import numpy
 from draw_game_board import draw_game_board
 from get_ai_move import get_ai_move
+from check_game import check_game
+from sklearn.neural_network import MLPClassifier
 
 print("Hello, i'm Python. Welcome to tic-tac-toe game. You will play against the AI")
 
@@ -22,33 +25,48 @@ else:
     machine_symbol = 'X'
 
 game_matrix = numpy.array([[0,0,0],[0,0,0],[0,0,0]])
+neural_network: MLPClassifier = joblib.load('./Esercizi/tic-tac-toe-ml/tic_tac_toe_mlp.pkl')
 
 #first turn
 while True:
     if machine_number == 1:
         #AI starts first
         ai_coordinates = get_ai_move(game_matrix, machine_symbol)
-        print(f"AI places {machine_symbol} at {ai_coordinates}")
         game_matrix[ai_coordinates[0], ai_coordinates[1]] = machine_number
+        print(f"AI places {machine_symbol} at {ai_coordinates}")
         print(draw_game_board(game_matrix))
-
-        player_coordinates = input(f"Human player, make your move. Where do you want to place your {player_symbol}? Use row,col starting from 1: ")
+        player_coordinates = input(f"Human player, make your move. Where do you want to place your {player_symbol}? Use row,col starting from 0: ")
         [player_x_coordinate, player_y_coordinate] = [int(player_coordinates.split(",")[0]), int(player_coordinates.split(",")[1])]
-
-    else:
-        #player first
-        player_coordinates = input(f"Human player, make your move. Where do you want to place your {player_symbol}? Use row,col starting from 1: ")
-        [player_x_coordinate, player_y_coordinate] = [int(player_coordinates.split(",")[0]), int(player_coordinates.split(",")[1])]
-
-        if game_matrix[player_x_coordinate-1, player_y_coordinate-1] != 0:
-            game_matrix[player_x_coordinate-1, player_y_coordinate-1] = player_number
+        if game_matrix[player_x_coordinate, player_y_coordinate] == 0:
+            game_matrix[player_x_coordinate, player_y_coordinate] = player_number
+            print(draw_game_board(game_matrix))
         else:
             print("Cell already occupied. Please select another cell")
+            continue
+    else:
+        #player first
+        player_coordinates = input(f"Human player, make your move. Where do you want to place your {player_symbol}? Use row,col starting from 0: ")
+        [player_x_coordinate, player_y_coordinate] = [int(player_coordinates.split(",")[0]), int(player_coordinates.split(",")[1])]
+
+        if game_matrix[player_x_coordinate, player_y_coordinate] == 0:
+            game_matrix[player_x_coordinate, player_y_coordinate] = player_number
+            print(draw_game_board(game_matrix))
+        else:
+            print("Cell already occupied. Please select another cell")
+            continue
 
         ai_coordinates = get_ai_move(game_matrix, machine_symbol)
-        print(f"AI places {machine_symbol} at {ai_coordinates}")
+        print(f"AI places {machine_symbol} at {ai_coordinates[0],ai_coordinates[1]}")
         game_matrix[ai_coordinates[0],ai_coordinates[1]] = machine_number
         print(draw_game_board(game_matrix))
-    
+    winner_number = check_game(game_matrix)
+    if winner_number in [1,2]:
+        print(f"Player {winner_number} wins")
+
+        if winner_number == 2:
+            neural_network = neural_network.fit(game_matrix)
+            joblib.dump(neural_network, './Esercizi/tic-tac-toe-ml/tic_tac_toe_mlp.pkl')
+        break
+
 
 #TODO:draw game board using pygame
